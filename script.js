69bd7c40eb1ed3d9824e52b194c40193733733c3
chrissundal <christoffersj@hotmail.com>
2024-09-22 22:49:46 +0200

    update

--- script.js
+++ script.js
@@ -1,15 +1,68 @@
-let pocketStuff = ['Hat', 'Pencil', 'Dinosaur', 'Showercap', 'Piece of Tape'];
-let imgFiles = ['IMG/hatt.jpg', 'IMG/blyant.jpg', 'IMG/dinosaur.jpg', 'IMG/showercap.jpg', 'IMG/tape.jpg']
-let prices = [49, 15, 149, 99, 35]
-let shoppingCart = []
-let showItem = '';
 let isOpen = false;
 let productHtml = '';
 let totalPrice = 0;
 let itemPrice = 0;
-
+let ShoppingCartCounter = 0;
+let shoppingCart = []
+let productItems = [
+    {
+        name: 'Hat',
+        img: 'IMG/hatt.jpg',
+        price: 49
+    },
+    {
+        name: 'Blyant',
+        img: 'IMG/blyant.jpg',
+        price: 15
+    },
+    {
+        name: 'Dinosaur',
+        img: 'IMG/dinosaur.jpg',
+        price: 149
+    },
+    {
+        name: 'Dusjhette',
+        img: 'IMG/showercap.jpg',
+        price: 99
+    },
+    {
+        name: 'Gaffatape',
+        img: 'IMG/tape.jpg',
+        price: 35
+    },
+    {
+        name: 'Banan',
+        img: 'IMG/banan.jpg',
+        price: 8
+    },
+    {
+        name: 'Dopapir',
+        img: 'IMG/dopapir.jpg',
+        price: 49
+    },
+    {
+        name: 'RedBull',
+        img: 'IMG/energi.jpg',
+        price: 35
+    },
+    {
+        name: 'Eple',
+        img: 'IMG/eple.jpg',
+        price: 7
+    },
+    {
+        name: 'Melk',
+        img: 'IMG/melk.jpg',
+        price: 22
+    },
+    {
+        name: 'Sjokolade',
+        img: 'IMG/sjokolade.jpg',
+        price: 35
+    },
+]
 updateView();
-function updateView(){
+function updateView() {
     document.getElementById('app').innerHTML = /*HTML*/`
     <div class="topHeader">
     <h1>Nettbutikk</h1>
@@ -17,77 +70,96 @@ function updateView(){
         <div class="grid">
         ${showProducts()}
         </div>
-    <div class="dropDown">
-        ${createPockets()}
-    </div>
+        ${createDropdown()}
+    <footer>
+        <p>Author: Chris</p>
+        <p><a href="mailto:christoffersj@hotmail.com">Kontakt oss</a></p>
+    </footer>
     
     `;
 }
-function createPockets(){
-    
+function createDropdown() {
     let html = '';
-    if (isOpen == false) return '<button class="cartButton" onclick="updateCart()">Handlekurv</button>';
-    else if (isOpen == true) { html = `
-        <div class="main">
-        <div>${shoppingCart}</div>
-        <div><strong>Totalt: ${totalPrice} kr</strong></div>
-        <button onclick="checkOut()">Checkout</button>
-        <button onclick="closePocket()">Close</button>
+    if (isOpen == false) return `
+    <div class="cartButtonDiv" onclick="openCart()">
+        <img src="IMG/cart.png" class="cartButton" height = 60px>
+        <div class="cartCounter">${ShoppingCartCounter}</div>
+    </div>
+    `;
+    else if (isOpen == true) {
+        html = `
+        <div class="dropDown">
+            <div class="main">
+                <div>${createCartItems()}</div>
+                <div><strong>Totalt: ${totalPrice} kr</strong></div>
+                <button onclick="checkOut()">Checkout</button>
+                <button onclick="closePocket()">Close</button>
+            </div>
         </div>
-        `; 
-    }  
+        `;
+    }
     return html;
 }
-
-function updateCart(){
-    isOpen = true; 
-    updateView();
+function createCartItems() {
+    let html = '';
+    for (let cartIndex = 0; cartIndex < shoppingCart.length; cartIndex++) {
+        html += `
+        <div class="innerCart">
+            <img src="${shoppingCart[cartIndex].img}" height = 50px width = 50px/>
+            <div>${shoppingCart[cartIndex].name}</div>
+            <div>Pris: ${shoppingCart[cartIndex].price} kr</div>
+            <button onclick="deleteItem(${cartIndex})">X</button>
+        </div>
+        `;
+    }
+    return html;
 }
-function showProducts(){
+function showProducts() {
     let showpro = '';
-    for (let index = 0; index < pocketStuff.length; index++) {
+    for (let index = 0; index < productItems.length; index++) {
         showpro += `
         <div class="products">
-        <div class="innerItem">
-        <div>${pocketStuff[index]}</div>
-        <img src="${imgFiles[index]}" height = 100px width = 100px/>
-        <div>Pris: ${prices[index]} kr</div>
-        <button onclick="addToCart(${index})">add to cart</button>
-        </div>
+            <div class="innerItem">
+                <div>${productItems[index].name}</div><br>
+                <div class="innerImg"><img src="${productItems[index].img}" height = 100px width = 100px/></div>
+                <div>Pris: ${productItems[index].price} kr</div>
+                <button class="addToCartBtn" onclick="addToCart(${index})">add to cart</button>
+            </div>
         </div>
         `;
     }
     return showpro;
 }
-
-function addToCart(index){
-    totalPrice += prices[index]
-    productHtml = `
-    <div class="cartItems">
-    <img src="${imgFiles[index]}" height = 30px width = 30px/>
-    <div> ${pocketStuff[index]}</div>
-    <div class="price">${prices[index]} kr</div>
-    </div>
-    `; 
-    shoppingCart.push(productHtml) 
+function deleteItem(cartIndex) {
+    totalPrice -= shoppingCart[cartIndex].price;
+    shoppingCart.splice(cartIndex, 1)
     updateView();
-    console.log(productHtml)
-    console.log(shoppingCart)
+}
+function openCart() {
+    isOpen = true;
+    updateView();
 }
 
-function closePocket(){
+function addToCart(index) {
+    totalPrice += productItems[index].price;
+    ShoppingCartCounter = shoppingCart.length + 1;
+    shoppingCart.push(productItems[index])
+    updateView();
+}
+
+function closePocket() {
     isOpen = false;
     updateView();
 }
-function checkOut(){
+function checkOut() {
     if (confirm('Betale nå?') == true) {
         shoppingCart = []
-        productHtml = '';
         totalPrice = 0;
+        ShoppingCartCounter = 0;
         isOpen = false;
         updateView();
-    }else{
+    } else {
         updateView();
     }
-    
+
 }
